1f937012ec47fa49516fe54d871e02cfc1be0ada
Andreas Herzog <andreas@novoky.com>
2020-10-15 09:00:46 +0200

    adds basic http authentication to GET /bikes endpoint

---
+++ src/main/java/org/ndrshrzg/bikerental/security/SessionSecurityConfig.java
@@ -0,0 +1,48 @@
+package org.ndrshrzg.bikerental.security;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.security.config.annotation.authentication.builders.AuthenticationManagerBuilder;
+import org.springframework.security.config.annotation.web.builders.HttpSecurity;
+import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
+import org.springframework.security.config.annotation.web.configuration.WebSecurityConfigurerAdapter;
+import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
+import org.springframework.security.crypto.password.PasswordEncoder;
+
+@Configuration
+@EnableWebSecurity
+public class SessionSecurityConfig extends WebSecurityConfigurerAdapter {
+
+    private static final String USERROLE = "USER";
+
+    @Autowired
+    public void configureGlobalAuth(AuthenticationManagerBuilder authBuilder) throws Exception {
+        authBuilder
+                .inMemoryAuthentication()
+                .withUser("User1")
+                .password(passwordEncoder()
+                        .encode("temp1234!")
+                ).roles(USERROLE);
+    }
+
+    @Override
+    protected void configure(HttpSecurity httpSecurity) throws Exception {
+        httpSecurity
+                .httpBasic()
+                .and()
+                .authorizeRequests()
+                // todo allow acces to /h2-console endpoint
+                // restrict access to other endpoints
+                .antMatchers("/bikes")
+                .hasRole(USERROLE)
+                .anyRequest().authenticated();
+    }
+
+
+    @Bean
+    PasswordEncoder passwordEncoder() {
+        return new BCryptPasswordEncoder();
+    }
+
+}
